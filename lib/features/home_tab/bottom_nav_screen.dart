a7d41f1014ac6c3704e2bb066d87e6b5d0f4e8e0
masihur <masihur96@gmail.com>
2025-11-19 11:53:21 +0600

    Project Setup

--- lib/features/home_tab/bottom_nav_screen.dart
+++ lib/features/home_tab/bottom_nav_screen.dart
@@ -2,6 +2,7 @@ import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:healthx_patient/core/constants/app_colors.dart';
 import 'package:healthx_patient/features/doctor/presentation/views/doctor_screen.dart';
+import 'package:healthx_patient/features/doctor/presentation/views/medicine_screen.dart';
 import 'package:healthx_patient/features/home_tab/view/home_screen.dart';
 import 'package:healthx_patient/features/lab_test/presentation/views/lab_test_screen.dart';
 import 'package:healthx_patient/features/profile/presentation/views/profile_screen.dart';
@@ -22,11 +23,7 @@ class _BottomNavScreenState extends State<BottomNavScreen> {
     HomeScreen(),
     DoctorScreen(),
     LabTestScreen(),
-
-    SizedBox(),
-    // PrescriptionScreen(
-    //   isForm: false,
-    // ),
+    MedicineScreen(),
     ProfileScreen(),
   ];
 
